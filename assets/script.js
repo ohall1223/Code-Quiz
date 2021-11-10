be45b3e3f5f62d1cb86d0348f42f1e892b3ca307
Olivia Hall <ohall1223@gmail.com>
2021-11-10 16:59:21 -0700

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+// an array of objects with questions and answers
+var questions = [
+    {
+        title: "Which of the following is NOT a valid data type in JavaScript",
+        choices: ["Boolean", "Number", "String", "Hedgehog"],
+        answer: "Hedgehog"
+    },
+    {
+        title: "Which tag is used to write JavaScript code",
+        choices: ["<script>", "<head>", "<main>", "<java>"],
+        answer: "<script>"
+    },
+    {
+        title: "A conditional statement is wrapped in...",
+        choices: ["Parentheses", "Curly Braces", "Brackets", "Quotes"],
+        answer: "Curly Braces"
+    },
+     {
+        title: "Which of the following built in function adds one or more elements to the end of an array?",
+        choices: ["pop()", "join()", "push()", "map()"],
+        answer: "push()"
+    },
+     {
+        title: "Which built-in method returns the character at the specified index?",
+        choices: ["charAt()", "getCharAt", "characterAt", "None of the above"],
+        answer: "charAt()"
+    },
+]
+
+// Declared variables
+var score = 0;
+var index = 0;
+
+var currentTime = 
